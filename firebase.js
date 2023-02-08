3f8532719ee7da7f532d1d16ad3b61c79a553e8b
Deniz <denizoz@live.com>
2023-02-08 14:38:33 +0300

    env variables work now

--- firebase.js
+++ firebase.js
@@ -1,22 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfigArr = [
-    import.meta.env.VITE_FIREBASE_API_KEY,
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    import.meta.env.VITE_FIREBASE_SENDER_ID,
-    import.meta.env.VITE_FIREBASE_APP_ID
-];
-//The above array is used to create the firebaseConfig object below
 const firebaseConfig = {
-    apiKey: firebaseConfigArr[0],
-    authDomain: firebaseConfigArr[1],
-    projectId: firebaseConfigArr[2],
-    storageBucket: firebaseConfigArr[3],
-    messagingSenderId: firebaseConfigArr[4],
-    appId: firebaseConfigArr[5]
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
